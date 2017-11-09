7c8952ee9e6311293c5e993d3611184d216a68ed
Matt Groff <mattlgroff@gmail.com>
2017-11-08 18:17:05 -0800

    Removed comment

--- config/connection.js
+++ config/connection.js
@@ -9,31 +9,14 @@ const source = {
     password: process.env.mysql_pw,
     database: "burgers_db"
   }
-  // ,
-  // // jawsDB
-  // jawsDB: {
-  //   port: 3306,
-  //   host: "<host name>",
-  //   user: "<name of user>",
-  //   password: "<password>",
-  //   database: "<name of database>"
-  // }
 };
 
-let connection = mysql.createConnection(
+//If JAWSDB, use JAWSDB. Otherwise, use localhost.
+const connection = mysql.createConnection(
   (process.env.JAWSDB_URL)
   ? process.env.JAWSDB_URL
   : source.localhost);
 
-// if(process.env.JAWSDB_URL){
-//   console.log("Using JAWSDB: " + process.env.JAWSDB_URL);
-//   let connection = mysql.createConnection(process.env.JAWSDB_URL);
-// }
-// else{
-//   console.log("Using localhost");
-//   // we use source.[name of connection] to hook into mysql
-// let connection = mysql.createConnection(source.localhost);
-// }
 
 connection.connect(err => {
   if (err) {
